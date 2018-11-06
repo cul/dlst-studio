e2f37472b636fa27cb275ba7d233118e689aa0f6
Alex Gil <colibri.alex@gmail.com>
2018-11-05 22:49:47 -0500

    cron job for 2018-11-05-22:49:47

--- cron.sh
+++ cron.sh
@@ -3,6 +3,6 @@ log=~/www/dhc-studio/cron.log
 d=$(date +%Y-%m-%d-%T)
 echo "Commit for $d" >> $log
 git add . 
-git commit -m "cron job for $d" &> $log
+git commit -m "cron job for $d" >>$log 2>>error
 echo " " >> $log 
 git push origin master
