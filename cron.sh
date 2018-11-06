bfc59dbb695aa4400b9137fefb84c10b701f6132
Alex Gil <colibri.alex@gmail.com>
2018-11-05 23:37:01 -0500

    Rebuild for 2018-11-05 23:37:00

--- cron.sh
+++ cron.sh
@@ -3,6 +3,7 @@ log=~/www/dhc-studio/cron.log
 d=$(date +%Y-%m-%d)
 t=$(date +%T)
 cd ~/www/dhc-studio/
+git config user.name "Alex Gil"
 git add . 
 git commit -m "Rebuild for $d $t" >>$log 2>>$log
 HOME=/home/alex git push origin master >>$log 2>>$log
