55576179a010cf5f4535a52b457a01fc15ace9fc
Alex Gil <colibri.alex@gmail.com>
2018-11-05 22:38:00 -0500

    cron job for 2018-11-05-22:38:00

---
+++ cron.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+rvm gemset use jekyll
+d=$(date +%Y-%m-%d-%T)
+echo "Commit for $d" > cron.log
+git add . 
+git commit -m "cron job for $d" &> cron.log
+echo " " > cron.log 
+git push origin master
