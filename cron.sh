1603e527d1a61c2793bbc3b6ad43996fc1c7890c
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-12-14 11:32:30 -0500

    Remove cron business.

--- cron.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-log=~/www/dhc-studio/cron.log
-d=$(date +%Y-%m-%d)
-t=$(date +%T)
-cd ~/www/dhc-studio/
-git add . 
-git commit -m "Rebuild for $d $t" >>$log 2>>$log
-HOME=/Users/alex git push origin master >>$log 2>>$log
-echo " " >> $log
