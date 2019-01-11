ca25a0ab040426bca4f3a3b1c781556f50b24bb0
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-01-11 13:28:18 -0500

    Don't add external links if the link has .no-ext

--- src/external-links.js
+++ src/external-links.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 export default function() {
   const externalLinkHTML =
     "<span>&nbsp;<i class='icon fa fa-small fa-external-link-alt'></i></span>";
-  $("a[href^='http']:not(a:has(img))").html(function(i, html) {
+  $("a[href^='http']:not(a:has(img)):not(.no-ext)").html(function(i, html) {
     if (!html.match("fa-external-link-alt")) {
       $(this).append($.parseHTML(externalLinkHTML));
     }
